9c88fedd0e8f70986e89f0f401d616239a873933
inraw <sahabatmewy@gmail.com>
2023-10-28 14:52:33 +0700

    first commit

---
+++ Config/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+    $koneksi = mysqli_connect("localhost","root","","keuangan");//("iplocal","username","password","nama database");
+
+    // Check Connection
+    if (mysqli_connect_errno()){
+        echo "Koneksi database gagal : " . mysqli_connect_error();
+    }else {
+        echo "Hore bisa konek";
+    }
+
+?>
---
+++ README.md
@@ -0,0 +1,3 @@
+# Pemograman1Tugas
+Ini adalah repo untuk tugas mata kuliah pemograman 1
+link youtube https://youtu.be/ExiAKqQddB0
